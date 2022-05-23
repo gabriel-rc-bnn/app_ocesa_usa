b23fa4979a68f527998869d123da3e1c3acf15c6
Gabriel Rosa Corona <gabokuck1990@gmail.com>
2022-05-23 18:02:18 -0500

    cambio de nombre de la app

--- android/app/src/main/java/com/ocesa/usa/app/MainActivity.java
+++ android/app/src/main/java/com/ocesa/usa/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ocesa.app;
+package com.ocesa.usa.app;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -18,6 +18,6 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "ocesa_app_mx";
+    return "appocesausa";
   }
 }
--- android/app/src/main/java/com/ocesa/usa/app/MainApplication.java
+++ android/app/src/main/java/com/ocesa/usa/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.ocesa.app;
+package com.ocesa.usa.app;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.ocesa.app.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.ocesa.usa.app.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
